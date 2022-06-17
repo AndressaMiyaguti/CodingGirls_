ca34c582ca30e3c1d24f3737b31a7354cc8c6c1d
AndressaMiyaguti <83734814+AndressaMiyaguti@users.noreply.github.com>
2022-06-17 17:07:47 -0300

    tarefas_aula05 Exercício 01 Introdução a OOP

---
+++ tarefas_aula05_introducao_OOP/Alunos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarefas_aula05_introducao_OOP
+{
+    class Alunos
+    {
+        /* 1 ) Escreva uma classe cujos objetos representam alunos matriculados em uma disciplina. 
+               Cada objeto dessa classe deve guardar os seguintes dados do aluno: matrícula, nome, 
+               2 notas de prova e 1 nota de trabalho. Escreva os seguintes métodos para esta classe:
+               - Calcular média: calcula e exibe a média final do aluno com base em suas notas;
+               - Calculo nota final: calcula quanto o aluno precisa tirar na prova final para ser aprovado.*/
+
+
+           //Dados solicitados
+
+            public int Matricula;
+            public string Nome;
+            public double Nota_prova01;
+            public double Nota_prova02;
+            public double Nota_trabalho;      
+        
+
+        public double Media()
+        {
+            // Calculo de MEDIA FINAL ( conforme pede o enunciado). Efetuado calculo por " 4 " pois é com base nelas que o aluno poderá ou não ser aprovado. 
+                        
+            return (Nota_prova01 + Nota_prova02 + Nota_trabalho) / 4;
+        }
+
+        public double Calculo_para_Aprov_Reprov()
+        {
+
+            //Calculo considerando 7 a nota mínima para Aprovação.
+            
+          //  double nota_projeto = 0;
+           
+           double diferenca = 0;
+           
+
+            if( Media() < 7)
+            {
+               diferenca = (7 - Media()) * 4;
+               Console.WriteLine($" Para que o aluno seja APROVADO a nota mínima necessária na ultima avaliação é  ");
+            }
+            else
+            {
+               Console.WriteLine(" Com base nas avaliações até o momento, o aluno está aprovado. Entretanto aconselhamos a execução da ultima prova");
+            }
+            return diferenca;
+        }
+
+        public override string ToString()
+        {
+
+            return " Nome do Alunos:" + Nome + "\n 1° Nota: " + Nota_prova01 + "\n 2° Nota: " + Nota_prova02 + "\n 3° Nota: " + Nota_trabalho +
+                "\n Média atual com base em 4 avaliações: " + Media() + "\n";
+        }
+    }
+}
---
+++ tarefas_aula05_introducao_OOP/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tarefas_aula05_introducao_OOP;
+using System.Globalization;
+
+namespace aula05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Task01();
+            
+
+
+        }
+        static void Task01()
+        {
+            /*
+             1 ) Escreva uma classe cujos objetos representam alunos matriculados em uma disciplina. 
+                 Cada objeto dessa classe deve guardar os seguintes dados do aluno: matrícula, nome, 
+                 2 notas de prova e 1 nota de trabalho. Escreva os seguintes métodos para esta classe:
+                 - Calcular média: calcula e exibe a média final do aluno com base em suas notas;
+                 - Calculo nota final: calcula quanto o aluno precisa tirar na prova final para ser aprovado.*/
+
+
+            Console.WriteLine("\n\n------------------------------------- Formulário Simples Cadastro de Alunos ----------------------------------\n\n");
+            Console.WriteLine("\n\n-------------------------------------- Insira os dados do Alunos ---------------------------------------------\n\n");
+
+            //variável para armazenar os atributos;
+            Alunos dadosAlunos = new Alunos();
+           
+            
+            Console.Write(" Nome: ");
+            dadosAlunos.Nome = Console.ReadLine().ToUpper();
+
+            Console.Write(" Matrícula: ");
+            dadosAlunos.Matricula = int.Parse(Console.ReadLine());
+
+            Console.Write(" Nota primeira prova : ");
+            dadosAlunos.Nota_prova01 = double.Parse(Console.ReadLine());
+
+            Console.Write(" Nota segunda prova : ");
+            dadosAlunos.Nota_prova02 = double.Parse(Console.ReadLine());
+
+            Console.Write(" Nota do projeto : ");
+            dadosAlunos.Nota_trabalho = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Dados do aluno: " + dadosAlunos );
+            Console.WriteLine( dadosAlunos.Calculo_para_Aprov_Reprov());
+
+            Console.ReadLine();
+
+
+
+
+
+
+            /* 2 ) Escreva uma classe em que cada objeto representa um vôo que acontece em determinada data 
+                 e em determinado horário. Cada vôo possui no máximo 100 passageiros, e a classe permite 
+                 controlar a ocupação das vagas.
+                 A classe deve ter os seguintes métodos:
+                 - Ocupar Vaga: ocupa determinada cadeira do vôo, cujo número é recebido como parâmetro, 
+                   e retorna verdadeiro se a cadeira ainda não estiver ocupada (operação foi bem sucedida) 
+                   e falso caso contrário
+                 - Vagas Livres: retorna o número de cadeiras vagas disponíveis (não ocupadas) no vôo;
+                 - Cadeira Livre: Retorna o número da próxima cadeira livre;
+                 - Ocupa: ocupa determinada cadeira do vôo, cujo número é recebido como parâmetro. 
+                   Caso a cadeira esteja ocupada exibe uma mensagem, caso esteja vazia, reserva e exibe uma 
+                   mensagem de confirmação;
+                 - Ver horário: Imprime a data e o horário do vôo.
+
+             3 )  Crie uma classe Calculadora, que possui como métodos a multiplicação, divisão, subtração e 
+                 - soma de dois valores e exibe para o usuário.*/
+
+
+        }
+
+    }
+}
