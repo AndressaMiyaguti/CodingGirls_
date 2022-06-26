c3828e891b890ea018f234df4649d8e2757e6c0a
AndressaMiyaguti <83734814+AndressaMiyaguti@users.noreply.github.com>
2022-06-26 02:56:19 -0300

    Exercicio01 aula 5

---
+++ Aula05_exercício01/Aluno.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+
+namespace Aula05_exercício01
+{
+    class Aluno
+    {
+
+        //Dados solicitados
+
+        public int Matricula;
+        public string Nome;
+        public double Nota_prova01;
+        public double Nota_prova02;
+        public double Nota_trabalho;
+
+
+        public double Media()
+        {
+            // Calculo de MEDIA FINAL ( conforme pede o enunciado). Efetuado calculo por " 4 " pois é com base nelas que o aluno poderá ou não ser aprovado. 
+
+            return (Nota_prova01 + Nota_prova02 + Nota_trabalho) / 4;
+        }
+
+        public double Calculo_para_Aprov_Reprov()
+        {
+            //Calculo considerando 7 a nota mínima para Aprovação.
+            double diferenca = (7 - Media()) * 4;
+
+            return diferenca;
+
+        }
+        public string resultadoFinal()
+        {
+            string mensagem;
+            if (Calculo_para_Aprov_Reprov() > 7.0)
+            {
+                mensagem = "Para que o aluno seja APROVADO a nota mínima necessária na ultima avaliação é " + Calculo_para_Aprov_Reprov().ToString("F2", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                mensagem = "Com base nas avaliações até o momento, o aluno está aprovado. Entretanto aconselhamos a execução da ultima prova";
+            }
+            return mensagem;
+        }
+
+        public override string ToString()
+        {
+
+            return " Nome do Alunos:" + Nome + "\n 1° Nota: " + Nota_prova01.ToString("F2", CultureInfo.InvariantCulture) + "\n 2° Nota: " + Nota_prova02.ToString("F2", CultureInfo.InvariantCulture) + "\n 3° Nota: " + Nota_trabalho.ToString("F2", CultureInfo.InvariantCulture) +
+                "\n Média atual com base em 4 avaliações: " + Media().ToString("F2", CultureInfo.InvariantCulture) + "\n" + resultadoFinal();
+        }
+
+    }
+}
---
+++ Aula05_exercício01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula05_exercício01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1) Escreva uma classe cujos objetos representam alunos matriculados em uma disciplina. 
+             * Cada objeto dessa classe deve guardar os seguintes dados do aluno: matrícula, nome, 
+             * 2 notas de prova e 1 nota de trabalho. Escreva os seguintes métodos para esta classe:
+             * Calcular média: calcula e exibe a média final do aluno com base em suas notas;
+             * Calculo nota final: calcula quanto o aluno precisa tirar na prova final para ser aprovado.*/
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------- Formulário Simples Cadastro de Alunos ----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------- Insira os dados do Alunos ---------------------------------------------");
+
+            //variável para armazenar os atributos;
+            Aluno dadosAluno = new Aluno();
+
+
+            Console.Write(" Nome: ");
+            dadosAluno.Nome = Console.ReadLine().ToUpper();
+
+            Console.Write(" Matrícula: ");
+            dadosAluno.Matricula = int.Parse(Console.ReadLine());
+
+            Console.Write(" Nota primeira prova : ");
+            dadosAluno.Nota_prova01 = double.Parse(Console.ReadLine());
+
+            Console.Write(" Nota segunda prova : ");
+            dadosAluno.Nota_prova02 = double.Parse(Console.ReadLine());
+
+            Console.Write(" Nota do projeto : ");
+            dadosAluno.Nota_trabalho = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Dados do aluno: \n" + dadosAluno);
+           // Console.WriteLine(dadosAluno.resultadoFinal());
+
+            Console.ReadLine();
+        }
+    }
+}
